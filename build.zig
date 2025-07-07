1bb4c979b74c39bced84cd2b9fa711e5822801b7
Jacob Humphreys - System Admin <jahumphreys2060@gmail.com>
2025-07-06 19:17:43 -0500

    try again

--- build.zig
+++ build.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("my_lib", .{
-        .source_file = .{ .path = "src/interface.zig" },
+    _ = b.addModule("interface", .{
+        .root_source_file = .{ 
+            .path = "src/interface.zig" },
     });
 }
